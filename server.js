826c7c61d50dda13420c098a3c0030330099b2b3
Avs-cd-bc <Avs.cd.bc@gmail.com>
2017-07-17 15:07:33 -0700

    adding request to riotAPI for champion data

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const path = require("path");
+const RiotApi = require("./server/riotApi.js");
 const app = express();
-
+const riotApi = new RiotApi(app);
 
 
 app.use(express.static(path.join(__dirname, "node_modules")));
 app.use(express.static(path.join(__dirname, "client")));
 
 
-
 require("./server/config/routes.js")(app);
-const RiotApi = require("./server/riotApi.js");
-const riotApi = new RiotApi(app);
+
 
 app.listen(5000, ()=>{
     console.log("Listening on port 5000");
--- server/riotApi.js
+++ server/riotApi.js
@@ -1,7 +1,12 @@
 const apiKey = require("./config/secrets.js");
+const request = require("request");
 class riotApi{
     constructor(app){
-        console.log(apiKey);
+        let champData;
+        request(`https://na1.api.riotgames.com/lol/static-data/v3/champions?locale=en_US&tags=all&dataById=false&api_key=${apiKey}`, (err, res, body) =>{
+            if(err) console.log(err);
+            else champData = body;
+        });
     }
 }
 
