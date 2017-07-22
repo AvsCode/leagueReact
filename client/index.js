d9932c0f424b404e9f9b3ceaa9e61d547187de4c
AvsCode <avsean@gmail.com>
2017-07-22 16:35:21 -0700

    adding react packages

---
+++ client/index.js
@@ -0,0 +1,11 @@
+class landingPage extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <h1>Hello World</h1>
+    )
+  }
+}
+ReactDOM.render(<landingPage/>, document.getElementById("app"));
