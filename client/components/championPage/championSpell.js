49f49fbcd75276f81b3d49e46783cf90e8bd05f9
AvsCode <avsean@gmail.com>
2017-08-22 22:22:21 -0700

    updating champion spell component

--- client/components/championPage/championSpell.js
+++ client/components/championPage/championSpell.js
@@ -4,13 +4,32 @@ import PropTypes from 'prop-types';
 export default class ChampionSpell extends React.Component{
     constructor(props){
         super(props);
+        this.spell = this.props.spell;
+        this.buildDisplayTooltip(this.spell);
     }
+    buildDisplayTooltip(spell){
+          spell.displayTooltip = this.spell.sanitizedTooltip.replace(/{{ e([1-9]) }}/g, ($1, $2) => {
+            return (`(${spell.effectBurn[$2]})`);
+          });
+
+          spell.displayTooltip = spell.displayTooltip.replace(/{{ (a[1-9]) }}/g, ($1, $2) => {
+            for(let i = 0; i < spell.vars.length; i++){
+              if(spell.vars[i].key === $2){
+                return(`${spell.vars[i].coeff[0]} x ${spell.vars[i].link}`);
+              }
+            }
+          });
+      }
     render(){
         console.log(this.props);
         return(
             <div className='championSpell'>
-                <h3>{this.props.spell.name}</h3>
-                <h4>{this.props.spell.sanitizedDescription}</h4>
+                <h3>{this.spell.name}</h3>
+                <h4>{this.spell.sanitizedDescription}</h4>
+                <div>Cost: {this.spell.costBurn} {this.spell.costType}</div>
+                <div>Range: {this.spell.rangeBurn}</div>
+                <div>Cooldown (Seconds): {this.spell.cooldownBurn}</div>
+                <div>{this.spell.displayTooltip}</div>
             </div>
         )
     }
--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,11 +1,19 @@
 import {createStore, applyMiddleware} from 'redux';
-import { composeWithDevTools } from 'remote-redux-devtools';
+// import { composeWithDevTools } from 'remote-redux-devtools';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
 export default function configureStore(initialState){
     return createStore(
         rootReducer,
-        initialState, composeWithDevTools(applyMiddleware(thunk))
+        initialState,
+        applyMiddleware(thunk)
     );
 }
+
+// export default function configureStore(initialState){
+//     return createStore(
+//         rootReducer,
+//         initialState, composeWithDevTools(applyMiddleware(thunk))
+//     );
+// }
